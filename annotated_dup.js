88b3e9a1743719a22c441746e2b1f6af1915d767
ojousima <ojousima@users.noreply.github.com>
2020-05-03 16:31:16 +0000

    Deploying to gh-pages from  @ 7ba37af91259256dec84d01d0de86fe96dfb34ea 🚀

--- annotated_dup.js
+++ annotated_dup.js
@@ -1,5 +1,5 @@
 var annotated_dup =
 [
-    [ "ruuvi_endpoint_3_data_t", "structruuvi__endpoint__3__data__t.html", "structruuvi__endpoint__3__data__t" ],
-    [ "ruuvi_endpoint_5_data_t", "structruuvi__endpoint__5__data__t.html", "structruuvi__endpoint__5__data__t" ]
+    [ "re_3_data_t", "structre__3__data__t.html", "structre__3__data__t" ],
+    [ "re_5_data_t", "structre__5__data__t.html", "structre__5__data__t" ]
 ];
