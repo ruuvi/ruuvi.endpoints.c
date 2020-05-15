c64add82dd121dc96f90cbcd1e9bfd6c08d45115
opsationby <opsationby@users.noreply.github.com>
2020-05-15 13:48:40 +0000

    Deploying to gh-pages from  @ 895e6a3160a0a9b82eb75228d4c7cd5419d88874 🚀

---
+++ structre__ibeacon__data__t.js
@@ -0,0 +1,7 @@
+var structre__ibeacon__data__t =
+[
+    [ "major", "structre__ibeacon__data__t.html#a600930655b7237315b72223c48327ea8", null ],
+    [ "minor", "structre__ibeacon__data__t.html#a9f280ce3ae5b6cd9346fd7a09ff29168", null ],
+    [ "proximity_uuid", "structre__ibeacon__data__t.html#a563fdda8fd1d9bdb2922f92860dfae74", null ],
+    [ "tx_power", "structre__ibeacon__data__t.html#a72a96ade6a01c9bd107a0977b6a3059c", null ]
+];
