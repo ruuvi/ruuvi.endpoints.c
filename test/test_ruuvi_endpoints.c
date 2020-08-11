da79f97cac554cc3db066142437bbb57ea3ab6bd
Otso Jousimaa <otso@ojousima.net>
2020-08-11 17:07:58 +0300

    Add data logging helpers

---
+++ test/test_ruuvi_endpoints.c
@@ -0,0 +1,16 @@
+#include "unity.h"
+
+#include "ruuvi_endpoints.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_ruuvi_endpoints_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement ruuvi_endpoints");
+}
