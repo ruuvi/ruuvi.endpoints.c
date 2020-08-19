fc86b20626dfd42d0948f2a5c7d758399be69a64
oleg.protasevich <oleg.protasevich@promwad.com>
2020-08-19 12:59:11 +0300

    [test]: astyle

--- test/test_ruuvi_endpoint_ca_uart.c
+++ test/test_ruuvi_endpoint_ca_uart.c
@@ -129,7 +129,7 @@ void test_ruuvi_endpoint_ca_uart_encode_null (void)
     re_ca_uart_cmd_t cmd = RE_CA_UART_SET_CH_37;
     re_ca_uart_ble_bool_t params =
     {
-	.state = 0x01U
+        .state = 0x01U
     };
     re_ca_uart_payload_t payload = {0};
     uint8_t size = 255U;
@@ -153,18 +153,18 @@ void test_ruuvi_endpoint_ca_uart_ack_encode (void)
         RE_CA_UART_STX,
         expected_size,
         RE_CA_UART_ACK,
-        RE_CA_UART_SET_ALL, 	
-	RE_CA_UART_FIELD_DELIMITER,
-	0x01U,
-	RE_CA_UART_FIELD_DELIMITER,
-	0x93U, 0xF1U, //crc
+        RE_CA_UART_SET_ALL,
+        RE_CA_UART_FIELD_DELIMITER,
+        0x01U,
+        RE_CA_UART_FIELD_DELIMITER,
+        0x93U, 0xF1U, //crc
         RE_CA_UART_ETX
     };
     re_ca_uart_cmd_t cmd = RE_CA_UART_ACK;
     re_ca_uart_ble_ack_t params =
     {
         .cmd = RE_CA_UART_SET_ALL,
-	.ack_state.state = 0x01
+        .ack_state.state = 0x01
     };
     re_ca_uart_payload_t payload = {0};
     payload.cmd = cmd;
@@ -186,18 +186,18 @@ void test_ruuvi_endpoint_ca_uart_ack_encode_invalid (void)
         RE_CA_UART_STX,
         expected_size,
         RE_CA_UART_ACK,
-        RE_CA_UART_SET_ALL, 	
-	RE_CA_UART_FIELD_DELIMITER,
-	0x01U,
-	RE_CA_UART_FIELD_DELIMITER,
-	0x93U, 0xF1U, //crc
+        RE_CA_UART_SET_ALL,
+        RE_CA_UART_FIELD_DELIMITER,
+        0x01U,
+        RE_CA_UART_FIELD_DELIMITER,
+        0x93U, 0xF1U, //crc
         RE_CA_UART_ETX
     };
     re_ca_uart_cmd_t cmd = RE_CA_UART_ACK;
     re_ca_uart_ble_ack_t params =
     {
         .cmd = RE_CA_UART_SET_ALL,
-	.ack_state.state = 0x01
+        .ack_state.state = 0x01
     };
     re_ca_uart_payload_t payload = {0};
     payload.cmd = cmd;
@@ -219,16 +219,14 @@ void test_ruuvi_endpoint_ca_uart_all_encode (void)
         RE_CA_UART_STX,
         expected_size,
         RE_CA_UART_SET_ALL,
-        0x01U, 0x01U, 	
-	RE_CA_UART_FIELD_DELIMITER,
-	0x7FU,
-	RE_CA_UART_FIELD_DELIMITER,
-	0xA1U, 0x9FU, //crc
+        0x01U, 0x01U,
+        RE_CA_UART_FIELD_DELIMITER,
+        0x7FU,
+        RE_CA_UART_FIELD_DELIMITER,
+        0xA1U, 0x9FU, //crc
         RE_CA_UART_ETX
     };
-
     re_ca_uart_cmd_t cmd = RE_CA_UART_SET_ALL;
-
     re_ca_uart_ble_all_t params = {0};
     params.fltr_id.id = 0x101;
     params.bools.fltr_tags.state = 1;
@@ -238,7 +236,6 @@ void test_ruuvi_endpoint_ca_uart_all_encode (void)
     params.bools.ch_37.state = 1;
     params.bools.ch_38.state = 1;
     params.bools.ch_39.state = 1;
-
     re_ca_uart_payload_t payload = {0};
     payload.cmd = cmd;
     payload.params.all_params = params;
@@ -259,16 +256,14 @@ void test_ruuvi_endpoint_ca_uart_all_encode_invalid (void)
         RE_CA_UART_STX,
         expected_size,
         RE_CA_UART_SET_ALL,
-        0x01U, 0x01U, 	
-	RE_CA_UART_FIELD_DELIMITER,
-	0xFFU,
-	RE_CA_UART_FIELD_DELIMITER,
-	0x39U, 0x84U, //crc
+        0x01U, 0x01U,
+        RE_CA_UART_FIELD_DELIMITER,
+        0xFFU,
+        RE_CA_UART_FIELD_DELIMITER,
+        0x39U, 0x84U, //crc
         RE_CA_UART_ETX
     };
-
     re_ca_uart_cmd_t cmd = RE_CA_UART_SET_ALL;
-
     re_ca_uart_ble_all_t params = {0};
     params.fltr_id.id = 0x101;
     params.bools.fltr_tags.state = 1;
@@ -278,7 +273,6 @@ void test_ruuvi_endpoint_ca_uart_all_encode_invalid (void)
     params.bools.ch_37.state = 1;
     params.bools.ch_38.state = 1;
     params.bools.ch_39.state = 1;
-
     re_ca_uart_payload_t payload = {0};
     payload.cmd = cmd;
     payload.params.all_params = params;
@@ -300,8 +294,8 @@ void test_ruuvi_endpoint_ca_uart_fltr_id_encode (void)
         expected_size,
         RE_CA_UART_SET_FLTR_ID,
         0x01U, 0x01U,
-	RE_CA_UART_FIELD_DELIMITER,
-	0xA8U, 0x39U, //crc
+        RE_CA_UART_FIELD_DELIMITER,
+        0xA8U, 0x39U, //crc
         RE_CA_UART_ETX
     };
     re_ca_uart_cmd_t cmd = RE_CA_UART_SET_FLTR_ID;
@@ -330,8 +324,8 @@ void test_ruuvi_endpoint_ca_uart_fltr_id_encode_invalid (void)
         expected_size,
         RE_CA_UART_SET_FLTR_ID,
         0x01U, 0x01U,
-	RE_CA_UART_FIELD_DELIMITER,
-	0xA8U, 0x39U, //crc
+        RE_CA_UART_FIELD_DELIMITER,
+        0xA8U, 0x39U, //crc
         RE_CA_UART_ETX
     };
     re_ca_uart_cmd_t cmd = RE_CA_UART_SET_FLTR_ID;
@@ -353,7 +347,6 @@ void test_ruuvi_endpoint_ca_uart_fltr_id_encode_invalid (void)
 void test_ruuvi_endpoint_ca_uart_bool_encode (void)
 {
     re_status_t err_code = RE_SUCCESS;
-
     const uint8_t expected_size = 2 + CMD_IN_LEN;
     uint8_t expected[] =
     {
@@ -361,14 +354,14 @@ void test_ruuvi_endpoint_ca_uart_bool_encode (void)
         expected_size,
         RE_CA_UART_SET_CH_37,
         0x01U,
-	RE_CA_UART_FIELD_DELIMITER,
-	0xB6U, 0x78U, //crc
+        RE_CA_UART_FIELD_DELIMITER,
+        0xB6U, 0x78U, //crc
         RE_CA_UART_ETX
     };
     re_ca_uart_cmd_t cmd = RE_CA_UART_SET_CH_37;
     re_ca_uart_ble_bool_t params =
     {
-	.state = 0x01U
+        .state = 0x01U
     };
     re_ca_uart_payload_t payload = {0};;
     payload.cmd = cmd;
@@ -384,7 +377,6 @@ void test_ruuvi_endpoint_ca_uart_bool_encode (void)
 void test_ruuvi_endpoint_ca_uart_bool_encode_invalid (void)
 {
     re_status_t err_code = RE_SUCCESS;
-
     const uint8_t expected_size = 2 + CMD_IN_LEN;
     uint8_t expected[] =
     {
@@ -392,14 +384,14 @@ void test_ruuvi_endpoint_ca_uart_bool_encode_invalid (void)
         expected_size,
         RE_CA_UART_SET_CH_37,
         0x01U,
-	RE_CA_UART_FIELD_DELIMITER,
-	0xB6U, 0x78U, //crc
+        RE_CA_UART_FIELD_DELIMITER,
+        0xB6U, 0x78U, //crc
         RE_CA_UART_ETX
     };
     re_ca_uart_cmd_t cmd = RE_CA_UART_SET_CH_37;
     re_ca_uart_ble_bool_t params =
     {
-	.state = 0x01U
+        .state = 0x01U
     };
     re_ca_uart_payload_t payload = {0};;
     payload.cmd = cmd;
@@ -695,14 +687,13 @@ void test_ruuvi_endpoint_ca_ack_decode (void)
         RE_CA_UART_STX,
         4 + CMD_IN_LEN,
         RE_CA_UART_ACK,
-        RE_CA_UART_SET_ALL, 	
-	RE_CA_UART_FIELD_DELIMITER,
-	0x01U,
-	RE_CA_UART_FIELD_DELIMITER,
-	0x93U, 0xF1U, //crc
+        RE_CA_UART_SET_ALL,
+        RE_CA_UART_FIELD_DELIMITER,
+        0x01U,
+        RE_CA_UART_FIELD_DELIMITER,
+        0x93U, 0xF1U, //crc
         RE_CA_UART_ETX
     };
-
     re_ca_uart_ble_ack_t expect_params = {0};
     expect_params.cmd = RE_CA_UART_SET_ALL;
     expect_params.ack_state.state = 1;
@@ -722,22 +713,21 @@ void test_ruuvi_endpoint_ca_uart_ack_decode_invalid (void)
         RE_CA_UART_STX,
         5 + CMD_IN_LEN,
         RE_CA_UART_ACK,
-        RE_CA_UART_SET_ALL, 	
-	RE_CA_UART_FIELD_DELIMITER,
-	RE_CA_ACK_ERROR, 
-	RE_CA_ACK_ERROR,
-	RE_CA_UART_FIELD_DELIMITER,
-	0x0BU, 0x41U, //crc
+        RE_CA_UART_SET_ALL,
+        RE_CA_UART_FIELD_DELIMITER,
+        RE_CA_ACK_ERROR,
+        RE_CA_ACK_ERROR,
+        RE_CA_UART_FIELD_DELIMITER,
+        0x0BU, 0x41U, //crc
         RE_CA_UART_ETX
     };
-
     re_ca_uart_ble_ack_t expect_params = {0};
     expect_params.cmd = RE_CA_UART_SET_ALL;
     expect_params.ack_state.state = 1;
     re_ca_uart_cmd_t expect_cmd = RE_CA_UART_ACK;
     re_ca_uart_payload_t payload = {0};
     err_code = re_ca_uart_decode (data, &payload);
-    TEST_ASSERT (RE_ERROR_DECODING_LEN == err_code); 
+    TEST_ASSERT (RE_ERROR_DECODING_LEN == err_code);
     TEST_ASSERT (memcmp (&expect_params, &payload.params.ack, sizeof (expect_params)));
     TEST_ASSERT (memcmp (&expect_cmd, &payload.cmd, sizeof (expect_cmd)));
 }
@@ -750,14 +740,13 @@ void test_ruuvi_endpoint_ca_uart_all_decode (void)
         RE_CA_UART_STX,
         5 + CMD_IN_LEN,
         RE_CA_UART_SET_ALL,
-        0x01U, 0x01U, 	
-	RE_CA_UART_FIELD_DELIMITER,
-	0xFFU,
-	RE_CA_UART_FIELD_DELIMITER,
-	0x39U, 0x84U, //crc
+        0x01U, 0x01U,
+        RE_CA_UART_FIELD_DELIMITER,
+        0xFFU,
+        RE_CA_UART_FIELD_DELIMITER,
+        0x39U, 0x84U, //crc
         RE_CA_UART_ETX
     };
-
     re_ca_uart_ble_all_t expect_params = {0};
     expect_params.fltr_id.id = 0x101;
     expect_params.bools.fltr_tags.state = 1;
@@ -767,12 +756,12 @@ void test_ruuvi_endpoint_ca_uart_all_decode (void)
     expect_params.bools.ch_37.state = 1;
     expect_params.bools.ch_38.state = 1;
     expect_params.bools.ch_39.state = 1;
-
     re_ca_uart_cmd_t expect_cmd = RE_CA_UART_SET_ALL;
     re_ca_uart_payload_t payload = {0};
     err_code = re_ca_uart_decode (data, &payload);
     TEST_ASSERT (RE_SUCCESS == err_code);
-    TEST_ASSERT (!memcmp (&expect_params, &payload.params.all_params, sizeof (expect_params)));
+    TEST_ASSERT (!memcmp (&expect_params, &payload.params.all_params,
+                          sizeof (expect_params)));
     TEST_ASSERT (!memcmp (&expect_cmd, &payload.cmd, sizeof (expect_cmd)));
 }
 
@@ -784,14 +773,13 @@ void test_ruuvi_endpoint_ca_uart_all_decode_invalid (void)
         RE_CA_UART_STX,
         6 + CMD_IN_LEN,
         RE_CA_UART_SET_ALL,
-        0x01U, 0x01U, 0x01U, 	
-	RE_CA_UART_FIELD_DELIMITER,
-	0x01U,
-	RE_CA_UART_FIELD_DELIMITER,
-	0x21U, 0x87U, //crc
+        0x01U, 0x01U, 0x01U,
+        RE_CA_UART_FIELD_DELIMITER,
+        0x01U,
+        RE_CA_UART_FIELD_DELIMITER,
+        0x21U, 0x87U, //crc
         RE_CA_UART_ETX
     };
-
     re_ca_uart_ble_all_t expect_params = {0};
     expect_params.fltr_id.id = 0x101;
     expect_params.bools.fltr_tags.state = 1;
@@ -801,7 +789,6 @@ void test_ruuvi_endpoint_ca_uart_all_decode_invalid (void)
     expect_params.bools.ch_37.state = 0;
     expect_params.bools.ch_38.state = 0;
     expect_params.bools.ch_39.state = 0;
-
     re_ca_uart_cmd_t expect_cmd = RE_CA_UART_SET_ALL;
     re_ca_uart_payload_t payload = {0};
     err_code = re_ca_uart_decode (data, &payload);
@@ -819,11 +806,10 @@ void test_ruuvi_endpoint_ca_id_decode (void)
         3 + CMD_IN_LEN,
         RE_CA_UART_SET_FLTR_ID,
         0x01U, 0x01U,
-	RE_CA_UART_FIELD_DELIMITER,
-	0xA8U, 0x39U, //crc
+        RE_CA_UART_FIELD_DELIMITER,
+        0xA8U, 0x39U, //crc
         RE_CA_UART_ETX
     };
-
     re_ca_uart_ble_fltr_id_t expect_params =
     {
         .id = 0x101
@@ -832,7 +818,8 @@ void test_ruuvi_endpoint_ca_id_decode (void)
     re_ca_uart_payload_t payload = {0};
     err_code = re_ca_uart_decode (data, &payload);
     TEST_ASSERT (RE_SUCCESS == err_code);
-    TEST_ASSERT (!memcmp (&expect_params, &payload.params.fltr_id_param, sizeof (expect_params)));
+    TEST_ASSERT (!memcmp (&expect_params, &payload.params.fltr_id_param,
+                          sizeof (expect_params)));
     TEST_ASSERT (!memcmp (&expect_cmd, &payload.cmd, sizeof (expect_cmd)));
 }
 
@@ -845,8 +832,8 @@ void test_ruuvi_endpoint_ca_uart_id_decode_invalid (void)
         4 + CMD_IN_LEN,
         RE_CA_UART_SET_FLTR_ID,
         0x01U, 0x01U, 0x01U,
-	RE_CA_UART_FIELD_DELIMITER,
-	0x6FU, 0x52U, //crc
+        RE_CA_UART_FIELD_DELIMITER,
+        0x6FU, 0x52U, //crc
         RE_CA_UART_ETX
     };
     re_ca_uart_ble_fltr_id_t expect_params =
@@ -857,22 +844,22 @@ void test_ruuvi_endpoint_ca_uart_id_decode_invalid (void)
     re_ca_uart_payload_t payload = {0};
     err_code = re_ca_uart_decode (data, &payload);
     TEST_ASSERT (RE_ERROR_DECODING_LEN == err_code);
-    TEST_ASSERT (memcmp (&expect_params, &payload.params.fltr_id_param, sizeof (expect_params)));
+    TEST_ASSERT (memcmp (&expect_params, &payload.params.fltr_id_param,
+                         sizeof (expect_params)));
     TEST_ASSERT (memcmp (&expect_cmd, &payload.cmd, sizeof (expect_cmd)));
 }
 
 void test_ruuvi_endpoint_ca_uart_bool_decode (void)
 {
     re_status_t err_code = RE_SUCCESS;
-
     uint8_t data[] =
     {
         RE_CA_UART_STX,
         2 + CMD_IN_LEN,
         RE_CA_UART_SET_CH_37,
         0x01U,
-	RE_CA_UART_FIELD_DELIMITER,
-	0xB6U, 0x78U, //crc
+        RE_CA_UART_FIELD_DELIMITER,
+        0xB6U, 0x78U, //crc
         RE_CA_UART_ETX
     };
     re_ca_uart_ble_bool_t expect_params = {0};
@@ -881,7 +868,8 @@ void test_ruuvi_endpoint_ca_uart_bool_decode (void)
     re_ca_uart_payload_t payload = {0};
     err_code = re_ca_uart_decode (data, &payload);
     TEST_ASSERT (RE_SUCCESS == err_code);
-    TEST_ASSERT (!memcmp (&expect_params, &payload.params.bool_param, sizeof (expect_params)));
+    TEST_ASSERT (!memcmp (&expect_params, &payload.params.bool_param,
+                          sizeof (expect_params)));
     TEST_ASSERT (!memcmp (&expect_cmd, &payload.cmd, sizeof (expect_cmd)));
 }
 
@@ -894,8 +882,8 @@ void test_ruuvi_endpoint_ca_uart_bool_decode_invalid (void)
         3 + CMD_IN_LEN,
         RE_CA_UART_SET_CH_37,
         0x01U, 0x00,
-	RE_CA_UART_FIELD_DELIMITER,
-	0xABU, 0x45U, //crc
+        RE_CA_UART_FIELD_DELIMITER,
+        0xABU, 0x45U, //crc
         RE_CA_UART_ETX
     };
     re_ca_uart_ble_bool_t expect_params = {0};
@@ -1069,7 +1057,7 @@ void test_ruuvi_endpoint_ca_uart_adv_decode_nodelimiter1 (void)
         0x35U, 0xC4U, 0x54U, 0x00U, 0x50U, 0x00U, 0xC8U, 0xFCU, 0x20U, 0xA4U, 0x56U,
         0xF0U, 0x30U, 0xE5U, 0xC9U, 0x44U, 0x54U, 0x29U, 0xE3U, 0x8DU, //!< Data
         0xD8U, //RSSI
-	0x9EU, 0x63U,//crc16
+        0x9EU, 0x63U,//crc16
         RE_CA_UART_ETX
     };
     re_ca_uart_ble_adv_t expect_params =
