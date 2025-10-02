a44e7e4274d617c27c6264ece1465a37fb25b89d
TheSomeMan <AlexeyN.Skvortsov@gmail.com>
2025-10-02 11:22:01 +0000

    Deploying to gh-pages from @ ruuvi/ruuvi.endpoints.c@8183d39ae8d0cd1c284881b10804af9b066b8b42 🚀

--- search/all_d.js
+++ search/all_d.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['secondary_5fphy_0',['secondary_phy',['../structre__ca__uart__ble__adv__t.html#afc9d43afa280bfdebcc1a3c18b5e4194',1,'re_ca_uart_ble_adv_t']]],
-  ['seq_5fcnt_1',['seq_cnt',['../structre__e1__data__t.html#a2aa77c3b96604df2761489c9d520819e',1,'re_e1_data_t']]],
+  ['seq_5fcnt_1',['seq_cnt',['../structre__e1__data__t.html#a85be3d25eda497422a2f4f6caa948903',1,'re_e1_data_t']]],
   ['seq_5fcnt2_2',['seq_cnt2',['../structre__6__data__t.html#a5e178010a98f4efe90d85fb95c7197d6',1,'re_6_data_t']]],
   ['sound_5favg_5fdba_3',['sound_avg_dba',['../structre__6__data__t.html#a7142b41c3c630b61471b3d0868fdf5f0',1,'re_6_data_t::sound_avg_dba'],['../structre__e0__data__t.html#a7142b41c3c630b61471b3d0868fdf5f0',1,'re_e0_data_t::sound_avg_dba'],['../structre__e1__data__t.html#a7142b41c3c630b61471b3d0868fdf5f0',1,'re_e1_data_t::sound_avg_dba'],['../structre__f0__data__t.html#a7142b41c3c630b61471b3d0868fdf5f0',1,'re_f0_data_t::sound_avg_dba']]],
   ['sound_5finst_5fdba_4',['sound_inst_dba',['../structre__e1__data__t.html#ae8be40b0fd6dbc1810dc739bd5033389',1,'re_e1_data_t']]],
--- structre__e1__data__t.js
+++ structre__e1__data__t.js
@@ -1,6 +1,6 @@
 var structre__e1__data__t =
 [
-    [ "address", "structre__e1__data__t.html#ab6b52376c1b9d81e6ea6f95a1225f832", null ],
+    [ "address", "structre__e1__data__t.html#ae3c5c60789e5f920f34f11a3010396b6", null ],
     [ "co2", "structre__e1__data__t.html#a410d6880f357f877b014f30a9aca31d1", null ],
     [ "flags", "structre__e1__data__t.html#a6c1874b7cad89eed92517df3d4ac8fcd", null ],
     [ "humidity_rh", "structre__e1__data__t.html#a62aa386606ead2e44bc56463b3f52a77", null ],
@@ -11,7 +11,7 @@ var structre__e1__data__t =
     [ "pm2p5_ppm", "structre__e1__data__t.html#a83f97f1f70325e8019a795c7b8730549", null ],
     [ "pm4p0_ppm", "structre__e1__data__t.html#a6f1666df0ba488041b5c68a93f2153b3", null ],
     [ "pressure_pa", "structre__e1__data__t.html#a6b53561bef0bcf81442f6ee13f077a17", null ],
-    [ "seq_cnt", "structre__e1__data__t.html#a2aa77c3b96604df2761489c9d520819e", null ],
+    [ "seq_cnt", "structre__e1__data__t.html#a85be3d25eda497422a2f4f6caa948903", null ],
     [ "sound_avg_dba", "structre__e1__data__t.html#a7142b41c3c630b61471b3d0868fdf5f0", null ],
     [ "sound_inst_dba", "structre__e1__data__t.html#ae8be40b0fd6dbc1810dc739bd5033389", null ],
     [ "sound_peak_spl_db", "structre__e1__data__t.html#a08d218f4d8fab6801bce998468545675", null ],
